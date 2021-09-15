2cbf5bbc4d22d41f41d829cde1ecc21820b3b183
guidota <guido@decentraland.org>
2021-09-15 16:19:09 -0300

    chore: separate API spec for each catalyst service

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { readFileSync }from 'fs'
 import { resolve } from 'path'
 import { parse } from 'yaml'
 
-const file = readFileSync(resolve(__dirname, './api.yaml'), 'utf8')
+const parseAPI = (fileName: string): any => {
+  const file = readFileSync(resolve(__dirname,'api', fileName), 'utf8')
+  return parse(file)
+}
 
-export const CATALYST_API = parse(file)
+export const CATALYST_API = parseAPI('catalyst.yaml')
+export const CONTENT_API = parseAPI('content.yaml')
+export const COMMS_API = parseAPI('comms.yaml')
+export const LAMBDAS_API = parseAPI('lambdas.yaml')
