9adbd4077ff7d97b0c09a249cebef7ca812fdb02
Hugo Arregui <hugo@decentraland.org>
2023-05-16 12:39:00 -0300

    use yarn, upgrade redocly

--- src/__tests__/validate-api.spec.ts
+++ src/__tests__/validate-api.spec.ts
@@ -2,26 +2,25 @@ import { CATALYST_API, CONTENT_API, COMMS_API, LAMBDAS_API } from '../../lib/ind
 import { OpenAPISchemaValidator } from 'express-openapi-validator/dist/framework/openapi.schema.validator'
 
 // This test is different to redocly lint
-describe("Validate OAS (Express integration)", () => {
-  
-  const validator = new OpenAPISchemaValidator({ version: '3', validateApiSpec: true})
+describe('Validate OAS (Express integration)', () => {
+  const validator = new OpenAPISchemaValidator({ version: '3', validateApiSpec: true })
 
-  it("Catalyst: should not be errors when validating API with schema validator", () => {
+  it('Catalyst: should not be errors when validating API with schema validator', () => {
     const result = validator.validate(CATALYST_API)
     expect(result.errors.length).toBe(0)
   })
 
-  it("Content: should not be errors when validating API with schema validator", () => {
+  it('Content: should not be errors when validating API with schema validator', () => {
     const result = validator.validate(CONTENT_API)
     expect(result.errors.length).toBe(0)
   })
 
-  it("Comms: should not be errors when validating API with schema validator", () => {
+  it('Comms: should not be errors when validating API with schema validator', () => {
     const result = validator.validate(COMMS_API)
     expect(result.errors.length).toBe(0)
   })
 
-  it("Lambdas: should not be errors when validating API with schema validator", () => {
+  it('Lambdas: should not be errors when validating API with schema validator', () => {
     const result = validator.validate(LAMBDAS_API)
     expect(result.errors.length).toBe(0)
   })
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import { readFileSync }from 'fs'
+import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import { parse } from 'yaml'
 
-const parseAPI = (fileName: string): any => {
-  const file = readFileSync(resolve(__dirname,'api', fileName), 'utf8')
+function parseAPI(fileName: string) {
+  const file = readFileSync(resolve(__dirname, 'api', fileName), 'utf8')
   return parse(file)
 }
 
 export const CATALYST_API = parseAPI('catalyst.yaml')
+
+function extractAPI(prefix: string): any {
+  const paths: Record<string, any> = {}
+
+  for (const k in paths) {
+    if (k.startsWith(prefix)) {
+      paths[k] = CATALYST_API.paths[k]
+    }
+  }
+  return { ...CATALYST_API, paths }
+}
+
+export const CONTENT_API = extractAPI('/content/')
+export const LAMBDAS_API = extractAPI('/lambdas/')
+export const COMMS_API = extractAPI('/comms/')
