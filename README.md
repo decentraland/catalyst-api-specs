c1670beb94623395322ce334b789c9936a040cf2
Hugo Arregui <hugo@decentraland.org>
2023-05-16 13:18:53 -0300

    chore: cleanup (#92)

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 # Catalyst Server API Definition
 
-API definition for the Catalyst Server implementation.  
+API definition for the Catalyst Server implementation.
 
-Check out the public API docs [here](https://decentraland.github.io/catalyst-api-specs/) 
+Check out the public API docs [here](https://decentraland.github.io/catalyst-api-specs/)
 
 ## Working on the Catalyst API Definition
 
 ### Install
 
 1. Install [Node JS](https://nodejs.org/).
-2. Clone this repo and run `npm install` in the repo root.
+2. Clone this repo and run `yarn` in the repo root.
 
 ### Usage
 
-#### `npm start`
+#### `yarn start`
 Starts the reference docs preview server.
 
-#### `npm run build`
+#### `yarn build`
 Bundles the definition to the dist folder.
 
-#### `npm test`
+#### `yarn test`
 Validates the definition.
 
 ## Readocly
@@ -30,5 +30,3 @@ tools including the lint tool and the reference
 docs engine.  Open it to find examples and
 [read the docs](https://redoc.ly/docs/cli/configuration/)
 for more information.
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import { readFileSync }from 'fs'
+import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import { parse } from 'yaml'
 
-const parseAPI = (fileName: string): any => {
-  const file = readFileSync(resolve(__dirname,'api', fileName), 'utf8')
+function parseAPI(fileName: string) {
+  const file = readFileSync(resolve(__dirname, fileName), 'utf8')
   return parse(file)
 }
 
-export const CATALYST_API = parseAPI('catalyst.yaml')
-export const CONTENT_API = parseAPI('content.yaml')
-export const COMMS_API = parseAPI('comms.yaml')
-export const LAMBDAS_API = parseAPI('lambdas.yaml')
+export const CATALYST_API = parseAPI('api.yaml')
+
+function extractAPI(prefix: string): any {
+  const paths: Record<string, any> = {}
+
+  for (const k in paths) {
+    if (k.startsWith(prefix)) {
+      paths[k] = CATALYST_API.paths[k]
+    }
+  }
+  return { ...CATALYST_API, paths }
+}
+
+export const CONTENT_API = extractAPI('/content/')
+export const LAMBDAS_API = extractAPI('/lambdas/')
+export const COMMS_API = extractAPI('/comms/')
