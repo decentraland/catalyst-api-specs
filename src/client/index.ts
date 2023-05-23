a1b6dacb568c256bbf784d0f0762f8e26522b46a
Hugo Arregui <hugo@decentraland.org>
2023-05-23 15:27:24 -0300

    Save Tue May 23 03:27:24 PM -03 2023

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,6 @@
 export * from './client.schemas'
 import { GetActiveEntities400 } from './client.schemas'
 
-// All content errors have the same schema, so we pick a random one
+// All errors have the same schema, so we pick a random one
 // to generate a generic type
-export type ContentErrorResponse = GetActiveEntities400
+export type ErrorResponse = GetActiveEntities400
