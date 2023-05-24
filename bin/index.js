59de8b6e3db012b66bea0dc9a11273603e73888c
Hugo Arregui <hugo@decentraland.org>
2023-05-24 14:00:24 -0300

    Save Wed May 24 02:00:24 PM -03 2023

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+async function main() {
+  const { generateJSONSchemaTypes, generateOpenAPITypes, toSource } = await import('schema2dts');
+  const openAPISchema = JSON.parse(readFileSync("lib/api.json").toString());
+  writeFileSync('src/API.d.ts', toSource(await generateOpenAPITypes(openAPISchema)));
+}
+
+
+main().catch(console.error)
