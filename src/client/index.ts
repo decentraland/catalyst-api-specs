e81da142a2a79bcb35a79e68f883b909110e2403
Hugo Arregui <hugo@decentraland.org>
2023-05-26 12:16:48 -0300

    chore: run orval over the bundled api (#108)

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,1 @@
 export * from './client.schemas'
-import { GetActiveEntities400, GetWearables503 } from './client.schemas'
-
-// All content errors have the same schema, so we pick a random one
-// to generate a generic type
-export type ContentErrorResponse = GetActiveEntities400
-
-// All lambdas (lamb2) errors have the same schema, so we pick a random one
-// to generate a generic type
-export type LambdasErrorResponse = GetWearables503
